0c1b5a5a54638ed11d74ba32f8ef85c727f3b3a3
Ilya <ilya_mukosey@mail.ru>
2022-02-01 18:19:38 +0300

    second db

--- Ucheba/js/script.js
+++ Ucheba/js/script.js
@@ -9,11 +9,29 @@ let personalMovieDB = {
     genders: [],
     privat: false 
 };
-let a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for(let i = 0; i< 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+        if (a != "" && b != "" && a == null && b == null && a.length < 50 && b.length < 50){
+            personalMovieDB.movies[a] = b;
+        } else{
+            i--;
+        }
+}
+
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+    
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+    
+} else if ( personalMovieDB.count >= 30){
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+
 console.log(personalMovieDB);
 
