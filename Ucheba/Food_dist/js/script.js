0f43a92862d007bb5b538116bfbf2f93769a15ce
Ilya <ilya_mukosey@mail.ru>
2022-03-10 20:19:40 +0300

    +slider

--- Ucheba/Food_dist/js/script.js
+++ Ucheba/Food_dist/js/script.js
@@ -203,13 +203,19 @@ window.addEventListener("DOMContentLoaded", () =>{
             return await res.json();
         };
 
-        getResource('http://localhost:3000/menu')
+        // getResource('http://localhost:3000/menu')
+        //     .then(data => {
+        //         data.forEach(({img, altimg, title, descr, price}) =>{
+        //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        //         });
+        //     });
+        axios.get("http://localhost:3000/menu")
             .then(data => {
-                data.forEach(({img, altimg, title, descr, price}) =>{
-                    new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+                data.data.forEach(({img, altimg, title, descr, price}) =>{
+                        new MenuCard(img, altimg, title, descr, price, '.menu .container')
+                        .render();
                 });
             });
-
         // Forms
 
         const forms = document.querySelectorAll("form");
@@ -301,4 +307,54 @@ window.addEventListener("DOMContentLoaded", () =>{
         //     .then(data => data.json())
         //     .then(res => console.log(res));
 
+    });
+
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total =  document.querySelector("#total"),
+          current =  document.querySelector("#current");
+    let slideIndex = 1;
+    
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n){
+            if(n > slides.length){
+            slideIndex = 1;
+            }
+            if (n < 1) {
+            slideIndex = slides.length;
+            }
+            slides.forEach(i => i.style.display = "none");
+            slides[slideIndex - 1].style.display ='block';
+
+            if (slideIndex < 10){
+                current.textContent = `0${slideIndex}`;
+            } else{
+                current.textContent = `${slideIndex}`;
+            }
+    }   
+
+    function plusSlides(n){
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', ()=>{
+        plusSlides(-1);
+    });
+    next.addEventListener('click', ()=>{
+        plusSlides(1);
     });
